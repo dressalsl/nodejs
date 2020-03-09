33ead94b5ef6c3a450e833bc1d054a5464e2f3f5
Andressa <dressalsl97@gmail.com>
2020-03-08 22:15:45 -0300

    inserir post

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const requireDir = require('require-dir');
 
 // Iniciando o app
 const app = express();
+app.use(express.json());
+
 
 // Iniciando o DataBase
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true});
+
 // com o npm install require-dir ele pega todos os models
 requireDir('./src/models');
 
